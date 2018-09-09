d11dedae6bc705c973ee05f9995db861d5928959
pedroamaral2812 <pedropaulo@life.com.br>
2018-09-09 15:27:15 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import Header from './src/components/Header';
+import PeopleList from './src/components/PeopleList';
+
+import axios from 'axios';
+
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    
+    this.state = {
+      peoples: []
+    };
+  }
+
+  componentDidMount(){
+    axios
+          .get('https://randomuser.me/api/?nat=br&results=5')
+          .then(response => {
+            const { results } = response.data;
+            this.setState({
+                peoples: results
+            }); 
+          })
+  }
+
+  render() {
+    return (
+      <View>
+        <Header title= "Pessoas!"/>
+        <PeopleList peoples={this.state.peoples}/>
+      </View>
+    );
+  }
+}
+
+
+
